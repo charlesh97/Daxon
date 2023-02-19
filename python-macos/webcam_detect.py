5132dfca116173f3a8ae456f801b1a2d853bb233
Charles Hood <charleshood@Charless-MacBook-Pro-16.local>
2023-02-18 21:39:12 -0800

    Working webcam with object detection using tflite

--- python-macos/webcam_detect.py
+++ python-macos/webcam_detect.py
@@ -7,6 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import sys
+import time
+
+from PIL import Image
+from six import BytesIO
+
 
 import tensorflow_hub as hub
 
@@ -18,69 +23,85 @@ print(tf.__version__)
 print('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))
 
 
-model = './lite-model_ssd_mobilenet_v2_fpn_100_fp32_default_1.tflite'
+model = './object_detection_mobile_object_localizer_v1_1_default_1.tflite' #'./lite-model_ssd_mobilenet_v1_100_320_uint8_default_1.tflite'
 enable_edgetpu = False
 num_threads = 1
 
 interpreter = tf.lite.Interpreter(model_path=model)
 interpreter.allocate_tensors()
 
-print(interpreter.get_signature_list())
-
 # Run our own data through a webcam
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
+last_time = time.time_ns()
 while True:
     # Read frame from camera
-    ret, image_np = cap.read()
-    image_np_resize = cv2.resize(image_np,(320, 320))
+    current_time = time.time_ns()
+    frame_time = (current_time - last_time)/1000000000
+    frame_rate = 1/frame_time
+    print(frame_rate)
+    last_time = current_time
 
-    input_image = tf.cast([image_np_resize], dtype=tf.float32)
-    print(input_image)
+    ret, image = cap.read()
+    image_resize = cv2.resize(image,(192, 192))
+
+    #image_data = tf.io.gfile.GFile('./person-walking-on-the-beach-with-a-dog.jpg', 'rb').read()
+    #image = Image.open(BytesIO(image_data))
+    #image_np = np.array(image.getdata()).reshape((1, 320, 320, 3)).astype(np.uint8)
+    #input_image = tf.cast(image_np, dtype=tf.uint8)
+
+    #image = Image.open(r"./person-walking-on-the-beach-with-a-dog.jpg")
+    #image = image.resize((192,192))
+    image_np = np.array(image_resize).reshape(1,192,192,3).astype(np.uint8)
+
+    #print('Image_np:',image_np.shape)
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
-    print('input:',input_details)
-    print('output:',output_details)
-    interpreter.set_tensor(input_details[0]['index'], input_image.numpy())
+    #print('input:',input_details)
+    #print('output:',output_details)
+
+    interpreter.set_tensor(input_details[0]['index'], image_np)
     # Invoke inference.
     interpreter.invoke()
+
+
     # Get the model prediction.
-    result = interpreter.get_tensor(output_details[0]['index']) #Gets box predictor
-    print(result.shape)
-    print(result)
+    output_dict = {
+        'num_detections': int(interpreter.get_tensor(output_details[3]["index"])),
+        'detection_classes': interpreter.get_tensor(output_details[1]["index"]).astype(np.uint8),
+        'detection_boxes' : interpreter.get_tensor(output_details[0]["index"]),
+        'detection_scores' : interpreter.get_tensor(output_details[2]["index"])
+    }
+
+    output_dict['detection_classes'] = output_dict['detection_classes'][0]
+    output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+    output_dict['detection_scores'] = output_dict['detection_scores'][0]
+
+    #print('detection_classes', output_dict['detection_classes'].shape)
+    #print('detection_boxes', output_dict['detection_boxes'].shape)
+    #print('detection_scores', output_dict['detection_scores'].shape)
     
-    # Image has dimensions of [x, y, 3], reduce to greyscale, default shrinks dimensions
-    #image_np_grey = np.mean(image_np_resize, axis=2) / 255.0
+    # Draw and process
+    for i in range(len(output_dict['detection_scores'])):
+        if output_dict['detection_scores'][i] > 0.5:
+            ymin = int(max(1,output_dict['detection_boxes'][i][0] * 192))
+            xmin = int(max(1,(output_dict['detection_boxes'][i][1] * 192)))
+            ymax = int(min(192,(output_dict['detection_boxes'][i][2] * 192)))
+            xmax = int(min(192,(output_dict['detection_boxes'][i][3] * 192)))
 
-    #img = (np.expand_dims(image_np_grey, axis=0))
-    
-    #predictions_single = probability_model.predict(img)
-    #print(class_names[np.argmax(predictions_single[0])])
+            cv2.rectangle(image_resize, (xmin,ymin), (xmax,ymax), (10,255,0), 1)
+            object_name = int(output_dict['detection_classes'][i])
+            label = '%s: %d%%' % (object_name, int(output_dict['detection_scores'][i]*100))
+            cv2.putText(image_resize, label, (xmin, ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255,255,255), 1) 
 
-
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    #image_np_expanded = np.expand_dims(image_np, axis=0)
-
-    # Things to try:
-    # Flip horizontally
-    # image_np = np.fliplr(image_np).copy()
-
-    # Convert image to grayscale
-    # image_np = np.tile(
-    #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
-
-    #input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-    #detections, predictions_dict, shapes = detect_fn(input_tensor)
-
-    #label_id_offset = 1
-    #image_np_with_detections = image_np.copy()
-
+            #print('Detection class' + str(output_dict['detection_classes'][i]) + 'with' + str(output_dict['detection_scores'][i]))
 
     # Display output
-    cv2.imshow('object detection', image)
+    cv2.imshow('object detection', image_resize)
+
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+       break
 
 cap.release()
 cv2.destroyAllWindows()
