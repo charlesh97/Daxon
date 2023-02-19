57d5da858db512b96267e8e4088c907619c72c75
Charles Hood <charleshood@Charless-MacBook-Pro-16.local>
2023-02-19 12:09:05 -0800

    Moved to mobile net SSD v1 with proper coco labeling. Completes the general premise of using tflite intepreter. Next step to build embedded gimbal motor solution

--- python-macos/webcam_detect.py
+++ python-macos/webcam_detect.py
@@ -22,16 +22,28 @@ import tensorflow_hub as hub
 print(tf.__version__)
 print('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))
 
-
-model = './object_detection_mobile_object_localizer_v1_1_default_1.tflite' #'./lite-model_ssd_mobilenet_v1_100_320_uint8_default_1.tflite'
+label_path = './coco17_labels.txt'
+model = './ssd_mobilenet_v1_1_metadata_1.tflite' #'./object_detection_mobile_object_localizer_v1_1_default_1.tflite' 
 enable_edgetpu = False
 num_threads = 1
 
+# Import the label data
+label = []
+with open(label_path, "r") as f:
+    for line in f.readlines():
+        label.append(line.rstrip())
+
+# Start up the interpreter
 interpreter = tf.lite.Interpreter(model_path=model)
 interpreter.allocate_tensors()
 
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+print('input:',input_details)
+print('output:',output_details)
+
 # Run our own data through a webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 last_time = time.time_ns()
 while True:
@@ -39,26 +51,21 @@ while True:
     current_time = time.time_ns()
     frame_time = (current_time - last_time)/1000000000
     frame_rate = 1/frame_time
-    print(frame_rate)
     last_time = current_time
 
     ret, image = cap.read()
-    image_resize = cv2.resize(image,(192, 192))
+    image_resize = cv2.resize(image,(300, 300))
 
     #image_data = tf.io.gfile.GFile('./person-walking-on-the-beach-with-a-dog.jpg', 'rb').read()
     #image = Image.open(BytesIO(image_data))
-    #image_np = np.array(image.getdata()).reshape((1, 320, 320, 3)).astype(np.uint8)
+    #image_np = np.array(image.getdata()).reshape((1, 300, 300, 3)).astype(np.uint8)
     #input_image = tf.cast(image_np, dtype=tf.uint8)
 
     #image = Image.open(r"./person-walking-on-the-beach-with-a-dog.jpg")
-    #image = image.resize((192,192))
-    image_np = np.array(image_resize).reshape(1,192,192,3).astype(np.uint8)
+    #image = image.resize((300,300))
+    image_np = np.array(image_resize).reshape(1,300,300,3).astype(np.uint8)
 
     #print('Image_np:',image_np.shape)
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    #print('input:',input_details)
-    #print('output:',output_details)
 
     interpreter.set_tensor(input_details[0]['index'], image_np)
     # Invoke inference.
@@ -77,28 +84,27 @@ while True:
     output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
     output_dict['detection_scores'] = output_dict['detection_scores'][0]
 
-    #print('detection_classes', output_dict['detection_classes'].shape)
-    #print('detection_boxes', output_dict['detection_boxes'].shape)
-    #print('detection_scores', output_dict['detection_scores'].shape)
+    #print('detection_classes', output_dict['detection_classes'])
+    #print('detection_boxes', output_dict['detection_boxes'])
+    #print('detection_scores', output_dict['detection_scores'])
     
     # Draw and process
     for i in range(len(output_dict['detection_scores'])):
-        if output_dict['detection_scores'][i] > 0.5:
-            ymin = int(max(1,output_dict['detection_boxes'][i][0] * 192))
-            xmin = int(max(1,(output_dict['detection_boxes'][i][1] * 192)))
-            ymax = int(min(192,(output_dict['detection_boxes'][i][2] * 192)))
-            xmax = int(min(192,(output_dict['detection_boxes'][i][3] * 192)))
+        if output_dict['detection_scores'][i] > 0.65:
+            ymin = int(max(1,output_dict['detection_boxes'][i][0] * 300))
+            xmin = int(max(1,(output_dict['detection_boxes'][i][1] * 300)))
+            ymax = int(min(300,(output_dict['detection_boxes'][i][2] * 300)))
+            xmax = int(min(300,(output_dict['detection_boxes'][i][3] * 300)))
 
             cv2.rectangle(image_resize, (xmin,ymin), (xmax,ymax), (10,255,0), 1)
-            object_name = int(output_dict['detection_classes'][i])
-            label = '%s: %d%%' % (object_name, int(output_dict['detection_scores'][i]*100))
-            cv2.putText(image_resize, label, (xmin, ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.25, (255,255,255), 1) 
+            object_name = label[int(output_dict['detection_classes'][i])]
+            image_label = '%s: %d%%' % (object_name, int(output_dict['detection_scores'][i]*100))
+            cv2.putText(image_resize, image_label, (xmin, ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (10,255,0), 1) 
+            cv2.putText(image_resize, 'fps:'+str(int(frame_rate)), (1,7), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255,255,255), 1)
 
-            #print('Detection class' + str(output_dict['detection_classes'][i]) + 'with' + str(output_dict['detection_scores'][i]))
 
     # Display output
     cv2.imshow('object detection', image_resize)
-
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
        break
